3b7454a22fd73f295ef50a5cc96efe21aa09a407
Gabriel Santana <115082578+Gabriellsantana@users.noreply.github.com>
2023-08-28 09:07:22 -0300

    add questão 05

--- main.py
+++ main.py
@@ -150,4 +150,49 @@ pessoa1.crescer(0.8)
 
 pessoa1.mostrar_informacoes()
 
+#5Classe Conta Corrente: Crie uma classe para implementar uma conta
+#corrente. A classe deve possuir os seguintes atributos: número da conta, nome
+#do correntista e saldo. Os métodos são os seguintes: alterarNome, depósito e
+#saque; No construtor, saldo é opcional, com valor default zero e os demais
+#atributos são obrigatórios.
+
+class ContaCorrente:
+    def __init__(self, numero_conta, nome_correntista, saldo=0):
+        self.numero_conta = numero_conta
+        self.nome_correntista = nome_correntista
+        self.saldo = saldo
+
+    def alterarNome(self, novo_nome):
+        self.nome_correntista = novo_nome
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente")
+
+    def mostrarInformacoes(self):
+        print(f"Número da Conta: {self.numero_conta}")
+        print(f"Nome do Correntista: {self.nome_correntista}")
+        print(f"Saldo: {self.saldo}")
+
+numero_conta_usuario = input("Digite o número da conta: ")
+nome_correntista_usuario = input("Digite o nome do correntista: ")
+
+conta1 = ContaCorrente(numero_conta_usuario, nome_correntista_usuario)
+
+conta1.mostrarInformacoes()
+
+valor_deposito = float(input("Digite o valor para depósito: "))
+conta1.deposito(valor_deposito)
+
+valor_saque = float(input("Digite o valor para saque: "))
+conta1.saque(valor_saque)
+
+conta1.mostrarInformacoes()
+
+
 
